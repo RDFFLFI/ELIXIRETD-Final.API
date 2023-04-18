b6f9472c8bccb48cfb5f6f52c3c8f4275f96f025
philip crasco <philiplorenzcrasco@gmail.com>
2023-04-18 09:18:20 +0800

    update miscellaneous Account titles

--- ELIXIRETD.DATA/DATA ACCESS LAYER/MODELS/INVENTORY_MODEL/MiscellaneousIssue.cs
+++ ELIXIRETD.DATA/DATA ACCESS LAYER/MODELS/INVENTORY_MODEL/MiscellaneousIssue.cs
@@ -21,6 +21,18 @@ namespace ELIXIRETD.DATA.DATA_ACCESS_LAYER.MODELS.INVENTORY_MODEL
         public bool IsActive { get; set; }
         public bool? IsTransact { get; set; }
 
+        public string CompanyCode { get; set; }
+        public string CompanyName { get; set; }
+
+        public string DepartmentCode { get; set; }
+        public string DepartmentName { get; set; }
+
+        public string LocationCode { get; set; }
+        public string LocationName { get; set; }
+
+        public string AccountCode { get; set; }
+        public string AccountTitles { get; set; }
+
     }
 
 
---
+++ ELIXIRETD.DATA/Migrations/20230418011502_addAccountTitlesMisc.cs
@@ -0,0 +1,175 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ELIXIRETD.DATA.Migrations
+{
+    public partial class addAccountTitlesMisc : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AccountCode",
+                table: "MiscellaneousIssueDetail");
+
+            migrationBuilder.DropColumn(
+                name: "AccountTitles",
+                table: "MiscellaneousIssueDetail");
+
+            migrationBuilder.DropColumn(
+                name: "CompanyCode",
+                table: "MiscellaneousIssueDetail");
+
+            migrationBuilder.DropColumn(
+                name: "CompanyName",
+                table: "MiscellaneousIssueDetail");
+
+            migrationBuilder.DropColumn(
+                name: "DepartmentCode",
+                table: "MiscellaneousIssueDetail");
+
+            migrationBuilder.DropColumn(
+                name: "DepartmentName",
+                table: "MiscellaneousIssueDetail");
+
+            migrationBuilder.DropColumn(
+                name: "LocationCode",
+                table: "MiscellaneousIssueDetail");
+
+            migrationBuilder.DropColumn(
+                name: "LocationName",
+                table: "MiscellaneousIssueDetail");
+
+            migrationBuilder.AddColumn<string>(
+                name: "AccountCode",
+                table: "MiscellaneousIssues",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "AccountTitles",
+                table: "MiscellaneousIssues",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CompanyCode",
+                table: "MiscellaneousIssues",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CompanyName",
+                table: "MiscellaneousIssues",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "DepartmentCode",
+                table: "MiscellaneousIssues",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "DepartmentName",
+                table: "MiscellaneousIssues",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LocationCode",
+                table: "MiscellaneousIssues",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LocationName",
+                table: "MiscellaneousIssues",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AccountCode",
+                table: "MiscellaneousIssues");
+
+            migrationBuilder.DropColumn(
+                name: "AccountTitles",
+                table: "MiscellaneousIssues");
+
+            migrationBuilder.DropColumn(
+                name: "CompanyCode",
+                table: "MiscellaneousIssues");
+
+            migrationBuilder.DropColumn(
+                name: "CompanyName",
+                table: "MiscellaneousIssues");
+
+            migrationBuilder.DropColumn(
+                name: "DepartmentCode",
+                table: "MiscellaneousIssues");
+
+            migrationBuilder.DropColumn(
+                name: "DepartmentName",
+                table: "MiscellaneousIssues");
+
+            migrationBuilder.DropColumn(
+                name: "LocationCode",
+                table: "MiscellaneousIssues");
+
+            migrationBuilder.DropColumn(
+                name: "LocationName",
+                table: "MiscellaneousIssues");
+
+            migrationBuilder.AddColumn<string>(
+                name: "AccountCode",
+                table: "MiscellaneousIssueDetail",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "AccountTitles",
+                table: "MiscellaneousIssueDetail",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CompanyCode",
+                table: "MiscellaneousIssueDetail",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CompanyName",
+                table: "MiscellaneousIssueDetail",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "DepartmentCode",
+                table: "MiscellaneousIssueDetail",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "DepartmentName",
+                table: "MiscellaneousIssueDetail",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LocationCode",
+                table: "MiscellaneousIssueDetail",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LocationName",
+                table: "MiscellaneousIssueDetail",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
